4c23f5c0220b899c339558bec47cdc217412f9be
teveztomas86-jpg <teveztomas86@gmail.com>
2026-05-04 09:51:39 -0300

    puerto dinamico Railway

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const { router: pagosRouter } = require('./routes/pagos');
 app.use('/', pedidosRouter);
 app.use('/pagos', pagosRouter);   // ← ahora sí lo registramos
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en puerto 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log('Servidor corriendo en puerto ' + PORT);
 });
